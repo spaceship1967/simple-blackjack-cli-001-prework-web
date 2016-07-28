18130c89f0598e504773a9cf71ed16def550601a
Michael Wiggins <michael.wiggins@me.com>
2016-07-28 20:30:01 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,75 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card = 1 + rand(11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card)
+#accepts one argument, the card total
+#prints the value of the cards to the screen
+#does not hardcode the card total
+  puts "Your cards add up to #{card}"
 end
 
 def prompt_user
-  # code #prompt_user here
+#gives instructions for hitting or staying
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+#prints apology, card total, and thank you message
+puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+#calls on #deal_card twice and returns the sum
+#calls on the '#display_card_total' to print sum of cards
+card = deal_card + deal_card
+display_card_total(card)
+card
 end
 
-def hit?
-  # code hit? here
+def hit?(card)
+# code hit? here
+prompt_user
+user_input = get_user_input
+
+if user_input == "h"
+card += deal_card
+
+elsif user_input == "s"
+ card
+
+ else
+ invalid_command
 end
+end
+
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card = hit?(initial_round)
+  until card > 21
+    display_card_total(card)
+    card += hit?(deal_card)
+    end
+    display_card_total(card)
+    end_game(card)
 end
-    
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -7,7 +7,7 @@ end
 
 describe "#deal_card" do
   it "generates a random number between 1-11" do
-    20.times do 
+    20.times do
       card = deal_card
       expect(card).to be > 0
       expect(card).to be < 12
@@ -106,11 +106,11 @@ describe "#runner" do
   before(:each) do
     def get_user_input
       "h"
-    end 
+    end
   end
 
-  it "calls on the #welcome method, 
-  then on the #initial_round method, 
+  it "calls on the #welcome method,
+  then on the #initial_round method,
   then calls #hit? and #display_card_total methods
   -until- the card sum is greater than 21,
   then calls on the #end_game method" do
